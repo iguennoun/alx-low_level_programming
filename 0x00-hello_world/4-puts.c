dbc501e04054f3df20507ff2848d295713a48ebe
GUENNOUN Ismail <ismail.guennoun@yahoo.fr>
2023-03-16 17:38:05 -0700

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: 0 - to stop the program
+ *
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
