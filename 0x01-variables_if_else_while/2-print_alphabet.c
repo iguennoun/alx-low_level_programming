cd61d130c2d4c2abdce4b3579e1c36a9d9562b3f
GUENNOUN Ismail <ismail.guennoun@yahoo.fr>
2023-03-17 18:09:19 -0700

    Task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+** main - Program begening
+**
+** Return: 0 to stop the program
+**
+*/
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z' ; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
