0b740cfe06e927995b4aa4e39c1e6e647ce7afe9
GUENNOUN Ismail <ismail.guennoun@yahoo.fr>
2023-03-17 18:42:49 -0700

    Task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+** main - Program begening
+**
+** Return: 0 to stop the program
+**
+*/
+int main(void)
+{
+	char mychar;
+
+	for (mychar = 'z' ; mychar <= 'a' ; mychar--)
+		putchar(mychar);
+	putchar('\n');
+	return (0);
+}
