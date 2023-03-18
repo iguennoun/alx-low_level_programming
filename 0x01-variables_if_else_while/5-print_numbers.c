d00b5b578270c267437f1f02bd1e4b30e86f4b92
GUENNOUN Ismail <ismail.guennoun@yahoo.fr>
2023-03-17 18:33:28 -0700

    Task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+** main - Program begening
+**
+** Return: 0 to stop the program
+**
+*/
+int main(void)
+{
+	int dig;
+
+	for (dig = 0 ; dig <= 10 ; dig++)
+		printf("%d", dig);
+	putchar('\n');
+	return (0);
+}
