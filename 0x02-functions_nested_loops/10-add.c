11b472799905da6d95a847a1eef83107dae63c42
GUENNOUN Ismail <ismail.guennoun@yahoo.fr>
2023-03-21 19:30:40 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+** add - A function to addition two integers
+** @n1: The first number
+** @n2: The second number
+** Return: Integer value of the addition
+**/
+
+int add(int n1, int n2)
+
+{
+	return (n1 + n2);
+}
