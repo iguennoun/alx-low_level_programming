225fd70d253793b02925fa40b0c29e1b3e5b6b9f
GUENNOUN Ismail <ismail.guennoun@yahoo.fr>
2023-03-27 16:43:36 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+** reset_to_98 -  Get a pointer to update an int
+** it points to to 98
+**
+** @n: Is an integer input pointer
+**
+** Return : nothing
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is the README.md file for "C - Pointer, arrays and strings" task by GI _ To be complemented later 
