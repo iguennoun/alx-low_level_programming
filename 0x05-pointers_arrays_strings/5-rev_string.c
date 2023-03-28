2cc5cb931958304da8fc73939a437cdafbd1f2a5
GUENNOUN Ismail <ismail.guennoun@yahoo.fr>
2023-03-27 17:15:37 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+** rev_string - reverse a string value
+**
+** @s: string to print
+*/
+
+void rev_string(char *s)
+{
+	int i;
+	char temp;
+
+	for (i = 0; i < _strlen(s) / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[_strlen(s) - i - 1];
+		s[_strlen(s) - i - 1] = temp;
+	}
+}
