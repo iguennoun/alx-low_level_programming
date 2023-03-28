520f52a9e9e54b82d793e43d4a8705d6ce31d7cb
GUENNOUN Ismail <ismail.guennoun@yahoo.fr>
2023-03-27 18:02:14 -0700

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+** _strcpy - Copy a string
+** @src: String value to copy
+** @dest: Destination of the copied value
+** Return: the pointer to dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i++] = '\0';
+	return (dest);
+}
