b3c82ff5bdce11db1f44c0d9c092aa49cf839d2f
GUENNOUN Ismail <ismail.guennoun@yahoo.fr>
2023-03-27 17:41:01 -0700

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+** puts_half - print half of a string
+** @str: the string to print
+** Return: void
+*/
+
+void puts_half(char *str)
+{
+	int i;
+
+	(_strlen(str) % 2 != 0) ? i = (_strlen(str) - 1) / 2 : _strlen(str) / 2;	
+	for (; i < _strlen(str); i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
