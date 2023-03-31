61d437054df7b1cbef2fe4e613962aa864b26d44
GUENNOUN Ismail <ismail.guennoun@yahoo.fr>
2023-03-30 17:07:38 -0700

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+/**
+** leet - encode a string aA 4,eE 3, oO 0, tT 7, lL 1
+** @n: input value
+** Return: n encoded value
+*/
+
+char *leet(char *n)
+{
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+	
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+			}
+		}
+	}
+	return (n);
+}
