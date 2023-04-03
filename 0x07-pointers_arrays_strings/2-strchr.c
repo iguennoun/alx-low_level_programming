115a2b6bd448068181335d2fc46ed15e3cdb4547
GUENNOUN Ismail <ismail.guennoun@yahoo.fr>
2023-04-03 16:57:56 -0700

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stddef.h>
 
 /**
 ** _strchr - Entry point
@@ -9,9 +9,9 @@
 
 char *_strchr(char *s, char c)
 {
-	int i = 0;
+	int i;
 
-	for (; s[i] != '\0'; i++)
+	for (i = 0 ; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 			return (s + i);
