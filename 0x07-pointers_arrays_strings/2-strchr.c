5d3322678e6fb87d22d73a22d67e722f863c5de2
GUENNOUN Ismail <ismail.guennoun@yahoo.fr>
2023-04-03 16:48:04 -0700

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include <stddef.h>
+/**
+** _strchr - Entry point
+** @s: string to searsh in
+** @c: the char to find
+** Return: the first occurrence of the char 'c' or NULL
+*/
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return (NULL);
+}
