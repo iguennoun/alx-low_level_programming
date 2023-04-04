2298eee3eeef8f1aa0a684ebe42aa5472ef355b5
GUENNOUN Ismail <ismail.guennoun@yahoo.fr>
2023-04-03 17:46:05 -0700

    Task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+/**
+** _strstr - Function that locate substring
+** @haystack: string to locate in
+** @needle: substring to lacate
+** Return: string at starting sub or null
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i,j;
+	
+	if (needle[0] == '\0')
+		return (haystack);
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] != needle[j])
+					break;
+			}
+			if (needle[j] == '\0')
+				return (haystack + i);
+		}
+	}
+	return ('\0');
+}
