cb7836a890030d5ed950f477034f6b6cc064389e
GUENNOUN Ismail <ismail.guennoun@yahoo.fr>
2023-04-03 17:32:09 -0700

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+/**
+** _strpbrk - Entry point
+** @s: input
+** @accept: input
+** Return: a pointer to byte in s if matches accept or null
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
+	}
+	return (0);
+}
