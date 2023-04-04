9f10095da66c3df655218ff8e7c5a215fde4cdd3
GUENNOUN Ismail <ismail.guennoun@yahoo.fr>
2023-04-03 17:56:18 -0700

    Task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+** print_chessboard - Board to print
+** @a: array
+** Return: print into screen
+**/
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
+	}
+}
