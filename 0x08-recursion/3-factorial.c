d2200d8b6e2adfa21844f0c99af1895be5f871e1
GUENNOUN Ismail <ismail.guennoun@yahoo.fr>
2023-04-05 16:47:10 -0700

    Task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+/**
+** factorial - returns the factorial of a number
+** @n: the number to calculate the factorial of
+**
+** Return: the factorial of n
+*/
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
