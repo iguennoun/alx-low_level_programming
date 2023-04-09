7f62742df0ee88b6e8a59b80e604c72aceff9a81
GUENNOUN Ismail <ismail.guennoun@yahoo.fr>
2023-04-09 16:26:00 -0700

    Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+** main - prints the sum of positive numbers in params + '\n'
+** @argc: Number of the program arguments
+** @argv: Array of the program arguments
+** Return: EXIT_SUCCESS = 0 if success of EXIT_FAILURE case else
+*/
+
+int main(int argc, char **argv)
+{
+	int i, sum = 0;
+
+	if (argc <= 1)
+	{
+		printf("0\n");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		while (argc-- > 1)
+		{
+			for (i = 0; *argv[i] != '\0'; i++)
+			{
+				if (!isdigit(*argv[i]))
+				{
+					printf("Error\n");
+					exit(EXIT_FAILURE);
+				}
+			}
+			sum += atoi(*argv);
+			(void) *argv++;
+		}
+		printf("%d\n", sum);
+		exit(EXIT_SUCCESS);
+	}
+}
