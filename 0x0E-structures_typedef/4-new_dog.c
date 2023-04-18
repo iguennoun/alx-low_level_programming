0c210fbed38bc7b19c35e479c352ef2c51e094e4
GUENNOUN Ismail <ismail.guennoun@yahoo.fr>
2023-04-17 18:30:51 -0700

    Task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+** new_dog - Creates a new dog
+** @name: The name of the new dog
+** @age: The age of the new dog
+** @owner: the name of the owner of the new dog
+**
+** Return: dog struct or NULL
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ndog;
+	int i, name_len, owner_len;
+
+	ndog = malloc(sizeof(dog_t));
+	if (!ndog)
+		exit(EXIT_FAILURE);
+
+	for (name_len = 0; name[name_len]; name_len++)
+
+	for (owner_len = 0; owner[owner_len]; owner_len++)
+
+	ndog->name = malloc(name_len + 1);
+	if (!ndog->name)
+	{
+		free(ndog);
+		exit(EXIT_FAILURE);
+	}
+	ndog->owner = malloc(owner_len + 1);
+	if (!ndog->owner)
+	{
+		free(ndog);
+		free(ndog->name);
+		exit(EXIT_FAILURE);
+	}
+
+	for (i = 0; i < name_len; i++)
+		ndog->name[i] = name[i];
+	ndog->name[i] = '\0';
+
+	ndog->age = age;
+
+	for (i = 0; i < owner_len; i++)
+		ndog->owner[i] = owner[i];
+	ndog->owner[i] = '\0';
+
+	return (ndog);
+}
