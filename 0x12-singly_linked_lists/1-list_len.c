0dfa1e0a37a18aeb521957183299912582b90388
GUENNOUN Ismail <ismail.guennoun@yahoo.fr>
2023-04-27 17:33:25 -0700

    Task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+** print_list - Calculate the number of element in a list_t list
+** @h: A pointer to the head node of the list
+**
+** Return: The number of elements in the list of type "struct list_s"
+*/
+
+size_t list_len(const list_t *h)
+{
+	size_t nbrelemlist;
+
+	nbrelemlist = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		nbrelemlist++;
+	}
+	return (nbrelemlist);
+}
