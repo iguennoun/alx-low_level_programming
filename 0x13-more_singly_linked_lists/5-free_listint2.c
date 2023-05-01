8d44373ed56dc97e1dfe6b65cb7e5682a33de976
GUENNOUN Ismail <ismail.guennoun@yahoo.fr>
2023-05-01 06:33:01 -0700

    Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a "listint_t" list
+ * @head: Head of a singly linked list
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *bucket_node;
+
+	if (head != NULL)
+	{
+		while (*head != NULL)
+		{
+			bucket_node = *head;
+			free(*head);
+			*head = bucket_node->next;
+		}
+
+		*head = NULL;
+
+	}
+}
